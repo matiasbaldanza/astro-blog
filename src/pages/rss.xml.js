0894eec6ab0850f54b4013602d04094ebc6585d0
Matias Baldanza <matiasbaldanza@gmail.com>
2024-01-17 11:32:41 -0300

    feat: update rss feed to use content collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const allPosts = await getCollection("posts");
   return rss({
     title: 'Astro Learner | Blog',
     description: 'A blog project to learn Astro',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./posts/*.md')),
+    items: allPosts.map((post) => ({
+      title: post.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
